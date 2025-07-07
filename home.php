0fb0e70556b3dc222442fb16fa123cd68370a1c2
Sonali Manjula <sonalijayakodi2002@gmail.com>
2025-07-07 12:41:02 +0530

    home update

--- home.php
+++ home.php
@@ -42,7 +42,8 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+
+   <link rel="stylesheet" href="css/home.css">
 
 </head>
 <body>
@@ -52,13 +53,14 @@ if(isset($_POST['add_to_cart'])){
 <section class="home">
 
    <div class="content">
-      <h3>Hand Picked Book to your door.</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quod? Reiciendis ut porro iste totam.</p>
-      <a href="about.php" class="white-btn">discover more</a>
+     <h3>Worlds of Wonder <span>Await You.</span></h3>
+
+      <p>Discover stories that inspire, educate, and transport you to new realms. Your next literary adventure is just a page away.</p>
+      <a href="about.php" class="white-btn">discover more <span class="arrow">  &rarr; </span></a>
    </div>
 
 </section>
-
+<br>
 <section class="products">
 
    <h1 class="title">latest products</h1>
@@ -70,16 +72,21 @@ if(isset($_POST['add_to_cart'])){
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
-     <form action="" method="post" class="box">
-      <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-      <div class="name"><?php echo $fetch_products['name']; ?></div>
-      <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-      <input type="number" min="1" name="product_quantity" value="1" class="qty">
+     <form action="" method="post" class="book-card">
+   <img class="book-image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+   <h3 class="book-title"><?php echo $fetch_products['name']; ?></h3>
+   <p class="book-author">by <?php echo $fetch_products['author'] ?? 'Unknown'; ?></p>
+
+  
+   <div class="book-bottom">
+      <div class="book-price">$<?php echo $fetch_products['price']; ?></div>
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-      <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-     </form>
+      <input type="submit" value="Add to Cart" name="add_to_cart" class="book-btn">
+   </div>
+</form>
+
       <?php
          }
       }else{
@@ -89,37 +96,36 @@ if(isset($_POST['add_to_cart'])){
    </div>
 
    <div class="load-more" style="margin-top: 2rem; text-align:center">
-      <a href="shop.php" class="option-btn">load more</a>
+      <a href="shop.php" class="option-btn">view All</a>
    </div>
 
 </section>
 
-<section class="about">
-
-   <div class="flex">
-
-      <div class="image">
-         <img src="images/about-img.jpg" alt="">
+<section class="about-alt">
+   <div class="about-wrapper">
+      <div class="about-text">
+         <h3>About Us</h3>
+         <p>
+            PAGETurner is a Sustainable & Ethically-Driven Bookstore for Readers.
+            Distinguished by its Curated Collections & Excellent Value, PAGETurner is
+            cherished by those who love knowledge, cherish stories, and seek comfort in pages.
+         </p>
+         <a href="about.php" class="about-btn">Learn More</a>
       </div>
-
-      <div class="content">
-         <h3>about us</h3>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
-         <a href="about.php" class="btn">read more</a>
+      <div class="about-image">
+         <img src="images/about.jpg" alt="Books on a shelf">
       </div>
-
    </div>
-
 </section>
+
+
 
 <section class="home-contact">
-
    <div class="content">
-      <h3>have any questions?</h3>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque cumque exercitationem repellendus, amet ullam voluptatibus?</p>
-      <a href="contact.php" class="white-btn">contact us</a>
+      <h3>Have any questions?</h3>
+      <p>Whether you're searching for your next favorite read, need help with an order, or want personalized book recommendations — we're here to help!</p>
+      <a href="contact.php" class="white-btn">Contact Us</a>
    </div>
-
 </section>
 
 
