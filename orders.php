74601972b5c8ec23881570998516dcab1053830d
Sonali Manjula <sonalijayakodi2002@gmail.com>
2025-07-09 20:32:10 +0530

    capital

--- orders.php
+++ orders.php
@@ -33,7 +33,7 @@ if(!isset($user_id)){
 
 <div class="heading">
    <h3>your orders</h3>
-   <p> <a href="home.php">home</a> / orders </p>
+   <p> <a href="home.php">Home</a> / Orders </p>
 </div>
 
 <section class="placed-orders">
